1eaac4695d8c7e944e98a5e5a33e860310ec8d39
maluroojas <127432742+maluroojas@users.noreply.github.com>
2023-08-03 16:13:03 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: implementation_imports, unused_import, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: implementation_imports, unused_import, prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -12,7 +12,37 @@ class PaginaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      routes: {
+        '/': (context) => Home(),
+        '/contador': (context) => Contador(),
+        '/curtir': (context) => Curtir(),
+      },
+    );
+  }
+}
+
+class Curtir extends StatelessWidget {
+  const Curtir({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Curtir",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            IconButton(
+              iconSize: 90,
+              icon: Icon(Icons.favorite_outline),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
@@ -22,8 +52,54 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String x = "100";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.calculate,
+              size: 36,
+            ),
+            title: Text("Calculadora"),
+            subtitle: Text("Ex de incremento e decremento"),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/contador');
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.favorite,
+              size: 36,
+            ),
+            title: Text("Curtir"),
+            subtitle: Text("Ex de curtir e descurtir"),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/curtir');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class Contador extends StatefulWidget {
+  const Contador({super.key});
+
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  int x = 100;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Meu App"),
@@ -32,17 +108,31 @@ class Home extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(x),
+            Text(x.toString()),
             ElevatedButton(
               onPressed: () {
-                print("Antes de zerar");
-                print(x);
-                x = "0";
-                print("Depois de zerar");
-                print(x);
+                setState(() {
+                  x = 0;
+                });
               },
               child: Text("zerar"),
-            )
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  x = x + 1;
+                });
+              },
+              child: Text("incrementar"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  x = x - 1;
+                });
+              },
+              child: Text("decrementar"),
+            ),
           ],
         ),
       ),
